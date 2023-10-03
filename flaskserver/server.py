6850294f584f9d246d36a8ef1f054705ca333fb7
BlocBoiMonzer <monzergmc1@gmail.com>
2023-10-03 14:01:30 +0200

    Update server.py

--- flaskserver/server.py
+++ flaskserver/server.py
@@ -4,10 +4,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-#Members API route
+# Read Members API route
 @app.route("/members")
 def members():
-    return {"members": ["Member1", "Member2", "Member3", "Member4", "Ali"]}
+    return {"members": ["Member1", "Member2", "Member3", "Member4", "Ali", "Hadi"]}
+
+
+# Begynn med å lage en funksjon for å legge til members
+
 
 if __name__ == "__main__":
     app.run(debug=True)

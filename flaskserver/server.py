d1f1321226c5aefd8c99bb686ef15e6408d22d58
D3S002 <abdulhma@hiof.no>
2023-10-20 21:58:34 +0200

    database

--- flaskserver/server.py
+++ flaskserver/server.py
@@ -1,19 +1,27 @@
 from flask import Flask, render_template, url_for, request, redirect, session, flash
 from flask_cors import CORS
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 app.secret_key = "hadi"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(days=5)
+
 
 # Read Members API route
 @app.route("/members")
 def members():
     return {"members": ["Member1", "Member2", "Member3", "Member4", "Ali", "Hadi"]}
 
-
 # Begynn med å lage en funksjon for å legge til members
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    
 
 @app.route("/select_tours")
 def select_tours():
@@ -41,11 +49,20 @@ def login():
         return render_template("login.html")
 
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return render_template("user.html", user=user)
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash("Email was saved!")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email=email, user=user)
     else:
         flash("you are not logged in!")
         return redirect(url_for("login"))
@@ -54,6 +71,7 @@ def user():
 @app.route("/logout")
 def logout():
     session.pop("user", None)
+    session.pop("email", None)
     flash("you have been logged out!", "info")
     return redirect(url_for("login"))
 

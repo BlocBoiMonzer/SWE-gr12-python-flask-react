b142e593418b48af7caf4d189f4d778af3707137
BlocBoiMonzer <monzergmc1@gmail.com>
2023-11-24 19:40:21 +0100

    fixed pyc ignore

---
+++ client/src/TourList.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+function TourList() {
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/tours')
+      .then(response => response.json())
+      .then(data => setTours(data));
+  }, []);
+
+  return (
+    <div>
+      {tours.map(tour => (
+        <div key={tour.id}>
+          <h2>{tour.name}</h2>
+          <p>{tour.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TourList;

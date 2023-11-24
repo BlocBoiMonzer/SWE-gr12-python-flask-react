343125b78ba7179171504ef4477ea8b08a76aa90
DankmemerXDD <kims9725@hotmail.com>
2023-11-24 14:34:24 +0100

    quick fixes på omdirigering av nettsidene
    .

--- flaskserver/models.py
+++ flaskserver/models.py
@@ -2,7 +2,7 @@ from extensions import db
 from datetime import datetime
 
 class User(db.Model):
-    ID = db.Column("ID", db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column("firstName", db.String(50))
     lastname = db.Column("lastName", db.String(50))
     phonenumber = db.Column("phoneNumber", db.Integer)
@@ -10,12 +10,13 @@ class User(db.Model):
     email = db.Column("email", db.String(100))
     username = db.Column("username", db.String(50), unique=True, nullable=False)
     password = db.Column("password", db.String(100), nullable=False)
+    bookings = db.relationship('Booking', backref='booking_user', lazy='dynamic')
 
 class Booking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     tour_id = db.Column(db.Integer, db.ForeignKey('tour.id'), nullable=False)
     booking_date = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.ID'))
     user = db.relationship('User', backref='user_bookings')
     tour = db.relationship('Tour', backref='tour_bookings')
 
